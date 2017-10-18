f87573b37620dad65441b2342138516d90aa8af3
g_bardaro <gianluca.bardaro@polimi.it>
2017-10-18 14:12:23 +0200

    small variation for node name

--- include/ros_base/ROSNode.h
+++ include/ros_base/ROSNode.h
@@ -10,56 +10,56 @@ sig_atomic_t volatile g_request_shutdown = 0;
 
 namespace ros_base {
 
-enum Errors {
-    NO_ERROR,
-    PARAM_ERROR,
-    SUB_FAILED,
-    PUB_FAILED,
-    INVALID_MESSAGE
-};
-
-class ROSNode : public StateMachine {
-private:
-    sig_atomic_t volatile g_error;
-    ros::AsyncSpinner spinner;
-    ros::ServiceClient stateService;
-    state_machine_msgs::SendState lastState;
-    
-    enum States {
-        ST_INIT,
-        ST_RUNNING,
-        ST_ERROR,
-        ST_CLOSING,
-        ST_MAX_STATES
+    enum Errors {
+        NO_ERROR,
+        PARAM_ERROR,
+        SUB_FAILED,
+        PUB_FAILED,
+        INVALID_MESSAGE
     };
-    
-    STATE_DECLARE(ROSNode, Init,    NoEventData)
-    STATE_DECLARE(ROSNode, Running, NoEventData)
-    STATE_DECLARE(ROSNode, Error,   NoEventData)
-    STATE_DECLARE(ROSNode, Closing, NoEventData)
+
+    class ROSNode : public StateMachine {
+    private:
+        sig_atomic_t volatile g_error;
+        ros::AsyncSpinner spinner;
+        ros::ServiceClient stateService;
+        state_machine_msgs::SendState lastState;
         
-    BEGIN_STATE_MAP
-        STATE_MAP_ENTRY(&Init)
-        STATE_MAP_ENTRY(&Running)
-        STATE_MAP_ENTRY(&Error)
-        STATE_MAP_ENTRY(&Closing) 
-    END_STATE_MAP
-    
-    bool initialize();
-    bool notifyState();
-public:
-    ROSNode();
-    void start();
-protected:
-    ros::NodeHandle handle;
-    
-    void setName(std::string name);
-    void faultDetected(Errors e);
-    bool noError();
-    bool virtual prepare() = 0;
-    void virtual errorHandling() = 0;
-    void virtual tearDown() = 0;
-};
+        enum States {
+            ST_INIT,
+            ST_RUNNING,
+            ST_ERROR,
+            ST_CLOSING,
+            ST_MAX_STATES
+        };
+        
+        STATE_DECLARE(ROSNode, Init,    NoEventData)
+        STATE_DECLARE(ROSNode, Running, NoEventData)
+        STATE_DECLARE(ROSNode, Error,   NoEventData)
+        STATE_DECLARE(ROSNode, Closing, NoEventData)
+            
+        BEGIN_STATE_MAP
+            STATE_MAP_ENTRY(&Init)
+            STATE_MAP_ENTRY(&Running)
+            STATE_MAP_ENTRY(&Error)
+            STATE_MAP_ENTRY(&Closing) 
+        END_STATE_MAP
+        
+        bool initialize();
+        bool notifyState();
+    public:
+        ROSNode();
+        void start();
+    protected:
+        ros::NodeHandle handle;
+        
+        void setName(std::string name);
+        void faultDetected(Errors e);
+        bool noError();
+        bool virtual prepare() = 0;
+        void virtual errorHandling() = 0;
+        void virtual tearDown() = 0;
+    };
 };
 
 #endif
