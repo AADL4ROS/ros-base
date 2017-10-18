65a8aeeb75bce72018dd4878f5b6400b720c442e
g_bardaro <gianluca.bardaro@polimi.it>
2017-10-18 14:11:59 +0200

    added configuration layout

---
+++ include/ros_base/Configuration.h
@@ -0,0 +1,20 @@
+#ifndef _CONFIGURATION_H
+#define _CONFIGURATION_H
+
+namespace ros_base {
+    struct ParametersBase {
+    };
+
+    struct VariablesBase {
+    };
+
+    class InternalStateBase {
+    protected:
+        std::shared_ptr<const ParametersBase> _params;
+        std::shared_ptr<VariablesBase> _vars;
+    public:
+        void virtual initialize(ParametersBase *p) = 0;
+    };
+}
+
+#endif
